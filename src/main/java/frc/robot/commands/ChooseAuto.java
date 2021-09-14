c8f5fe8a08fe10f3002a60af9088a55330f83cd6
WWLaptop <WWgithub@wiredwizards.org>
2021-09-13 20:04:13 -0400

    Autonomous command chooser
    adding an autonomous command chooser, not working yet.

--- src/main/java/frc/robot/commands/ChooseAuto.java
+++ src/main/java/frc/robot/commands/ChooseAuto.java
@@ -1,4 +1,9 @@
+package frc.robot.commands;
 
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandBase;
 
 public class ChooseAuto extends CommandBase {
     private final Command simpleDrive = new SimpleDrive();
